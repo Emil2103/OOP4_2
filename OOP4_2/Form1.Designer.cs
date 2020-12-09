596fa7522361015b3065d1d4e38e5fa0f5d9e56e
Emil Saubanov <emilsaubanov2013@gmail.com>
2020-12-09 13:22:37 +0500

    commit1

---
+++ OOP4_2/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace OOP4_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Num1 = new System.Windows.Forms.TextBox();
+            this.textBox_Num2 = new System.Windows.Forms.TextBox();
+            this.numericUpDown_Num1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown_Num2 = new System.Windows.Forms.NumericUpDown();
+            this.label_Num1 = new System.Windows.Forms.Label();
+            this.label_Num2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Num1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Num2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox_Num1
+            // 
+            this.textBox_Num1.Location = new System.Drawing.Point(78, 62);
+            this.textBox_Num1.Name = "textBox_Num1";
+            this.textBox_Num1.Size = new System.Drawing.Size(120, 22);
+            this.textBox_Num1.TabIndex = 0;
+            this.textBox_Num1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_Num1_KeyDown);
+            // 
+            // textBox_Num2
+            // 
+            this.textBox_Num2.Location = new System.Drawing.Point(364, 62);
+            this.textBox_Num2.Name = "textBox_Num2";
+            this.textBox_Num2.Size = new System.Drawing.Size(120, 22);
+            this.textBox_Num2.TabIndex = 1;
+            this.textBox_Num2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_Num2_KeyDown);
+            // 
+            // numericUpDown_Num1
+            // 
+            this.numericUpDown_Num1.Location = new System.Drawing.Point(78, 105);
+            this.numericUpDown_Num1.Name = "numericUpDown_Num1";
+            this.numericUpDown_Num1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown_Num1.TabIndex = 2;
+            this.numericUpDown_Num1.ValueChanged += new System.EventHandler(this.numericUpDown_Num1_ValueChanged);
+            // 
+            // numericUpDown_Num2
+            // 
+            this.numericUpDown_Num2.Location = new System.Drawing.Point(364, 105);
+            this.numericUpDown_Num2.Name = "numericUpDown_Num2";
+            this.numericUpDown_Num2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown_Num2.TabIndex = 3;
+            this.numericUpDown_Num2.ValueChanged += new System.EventHandler(this.numericUpDown_Num2_ValueChanged);
+            // 
+            // label_Num1
+            // 
+            this.label_Num1.AutoSize = true;
+            this.label_Num1.Location = new System.Drawing.Point(78, 144);
+            this.label_Num1.Name = "label_Num1";
+            this.label_Num1.Size = new System.Drawing.Size(0, 17);
+            this.label_Num1.TabIndex = 4;
+            // 
+            // label_Num2
+            // 
+            this.label_Num2.AutoSize = true;
+            this.label_Num2.Location = new System.Drawing.Point(364, 144);
+            this.label_Num2.Name = "label_Num2";
+            this.label_Num2.Size = new System.Drawing.Size(0, 17);
+            this.label_Num2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_Num2);
+            this.Controls.Add(this.label_Num1);
+            this.Controls.Add(this.numericUpDown_Num2);
+            this.Controls.Add(this.numericUpDown_Num1);
+            this.Controls.Add(this.textBox_Num2);
+            this.Controls.Add(this.textBox_Num1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Num1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Num2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Num1;
+        private System.Windows.Forms.TextBox textBox_Num2;
+        private System.Windows.Forms.NumericUpDown numericUpDown_Num1;
+        private System.Windows.Forms.NumericUpDown numericUpDown_Num2;
+        private System.Windows.Forms.Label label_Num1;
+        private System.Windows.Forms.Label label_Num2;
+    }
+}
+
---
+++ OOP4_2/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP4_2
+{
+    public partial class Form1 : Form
+    {
+        //public int a = 35;
+        //public int b = 78;
+        Model model;
+        public Form1()
+        {
+            InitializeComponent();
+           // textBox_Num1.Text = a.ToString();
+           // numericUpDown_Num1.Value = a;
+           // label_Num1.Text = a.ToString();
+           // textBox_Num2.Text = b.ToString();
+           // numericUpDown_Num2.Value = b;
+           // label_Num2.Text = b.ToString();
+            model = new Model();
+            
+            model.observers += new System.EventHandler(this.UpdateFromModel);
+            model.observers.Invoke(model, null);
+        }
+
+        
+
+        private void textBox_Num1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                textBox_Num2.Text = model.chekString(textBox_Num2.Text);
+                model.setValue1(Int32.Parse(textBox_Num1.Text), Int32.Parse(textBox_Num2.Text));
+            }
+
+               /* if (e.KeyCode == Keys.Enter)
+                {
+                    if (textBox_Num2.Text == "" || Int32.Parse(textBox_Num1.Text) < Int32.Parse(textBox_Num2.Text))
+                    {
+                        numericUpDown_Num1.Value = Int32.Parse(textBox_Num1.Text);
+                        label_Num1.Text = textBox_Num1.Text;
+                    }
+                    else
+                        MessageBox.Show("Введите значение меньше чем справа");
+                }*/
+        }
+
+        private void numericUpDown_Num1_ValueChanged(object sender, EventArgs e)
+        {
+            model.setValue1(Decimal.ToInt32(numericUpDown_Num1.Value), Decimal.ToInt32(numericUpDown_Num2.Value));
+
+            /*if (numericUpDown_Num2.Value == 0 || numericUpDown_Num1.Value < numericUpDown_Num2.Value)
+            {
+                textBox_Num1.Text = numericUpDown_Num1.Value.ToString();
+                label_Num1.Text = numericUpDown_Num1.Value.ToString();
+            }
+            else
+                MessageBox.Show("Введите значение меньше чем справа");*/
+        }
+
+        private void textBox_Num2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                textBox_Num1.Text = model.chekString(textBox_Num1.Text);
+                model.setValue2(Int32.Parse(textBox_Num1.Text), Int32.Parse(textBox_Num2.Text));
+            }
+
+            /*if (e.KeyCode == Keys.Enter)
+            {
+                if (textBox_Num1.Text == "" || Int32.Parse(textBox_Num2.Text) > Int32.Parse(textBox_Num1.Text))
+                {
+                    numericUpDown_Num2.Value = Int32.Parse(textBox_Num2.Text);
+                    label_Num2.Text = textBox_Num2.Text;
+                }
+                else
+                    MessageBox.Show("Введите значение больше чем слева");
+            }*/
+        }
+
+        private void numericUpDown_Num2_ValueChanged(object sender, EventArgs e)
+        {
+            model.setValue2(Decimal.ToInt32(numericUpDown_Num1.Value), Decimal.ToInt32(numericUpDown_Num2.Value));
+
+            /* if (numericUpDown_Num1.Value == 0 || numericUpDown_Num2.Value > numericUpDown_Num1.Value)
+             {
+                 textBox_Num2.Text = numericUpDown_Num2.Value.ToString();
+                 label_Num2.Text = numericUpDown_Num2.Value.ToString();
+             }
+             else
+                 MessageBox.Show("Введите значение больше чем слева");*/
+        }
+        private void UpdateFromModel(object sender, EventArgs e)
+        {
+            textBox_Num1.Text = model.getValue1().ToString();
+            numericUpDown_Num1.Value = model.getValue1();
+            label_Num1.Text = model.getValue1().ToString();
+
+            textBox_Num2.Text = model.getValue2().ToString();
+            numericUpDown_Num2.Value = model.getValue2();
+            label_Num2.Text = model.getValue2().ToString();
+        }
+    }
+
+    public class Model
+    {
+
+        public int value1 = 35;
+        public int value2 = 78;
+        public System.EventHandler observers;
+            
+        public void setValue1(int value1, int value2)
+        {
+            if (value2 > value1 || value2 == 0)
+                this.value1 = value1;
+            else
+                MessageBox.Show("Введите число меньше чем справа");
+            observers.Invoke(this, null);
+        }
+        
+        public void setValue2(int value1, int value2)
+        {
+            if (value2 > value1 || value1 == 0)
+            {
+                    this.value2 = value2;
+            }
+            else
+                MessageBox.Show("Введите число больше чем слева");
+            observers.Invoke(this, null);
+        }
+
+        public string chekString(string value)
+        {
+            if (value == "")
+                value = "0";
+            return value;
+        }
+
+        public int getValue1()
+        {
+            return value1;
+        }
+
+        public int getValue2()
+        {
+            return value2;
+        }
+    }
+}
